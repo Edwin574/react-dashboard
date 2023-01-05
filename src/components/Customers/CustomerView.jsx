1114fe268635490ed10826d837008fb0e65f6fc6
Edwin574 <owinoedwin574@gmail.com>
2023-01-05 14:04:46 +0300

    fetching users data

--- src/components/Customers/CustomerView.jsx
+++ src/components/Customers/CustomerView.jsx
@@ -11,10 +11,10 @@ function Customers() {
   useEffect(() => {
     async function getData() {
       await axios
-        .get("http://127.0.0.1:8000/easymove/customers/")
+        .get("http://127.0.0.1:8000/auth/users")
         .then((response) => {
           // check if the data is populated
-          console.log(response.data.results);
+          console.log(response.data);
           setData(response.data.results);
           // you tell it that you had the result
           setLoadingData(false);
@@ -26,50 +26,51 @@ function Customers() {
     }
   }, []);
 
-  const columns = [
-    {
-      field: "id",
-      headerName: "ID",
-      width: 50,
-    },
-    {
-      field: "address",
-      headerName: "Address",
-      width: 150,
-    },
-    {
-      field: "birth_date",
-      headerName: "DOB",
-      width: 150,
-    },
-    {
-      field: "sex",
-      headerName: "Gender",
-      width: 100,
-    }]
+  // const columns = [
+  //   {
+  //     field: "id",
+  //     headerName: "ID",
+  //     width: 50,
+  //   },
+  //   {
+  //     field: "address",
+  //     headerName: "Address",
+  //     width: 150,
+  //   },
+  //   {
+  //     field: "birth_date",
+  //     headerName: "DOB",
+  //     width: 150,
+  //   },
+  //   {
+  //     field: "sex",
+  //     headerName: "Gender",
+  //     width: 100,
+  //   }]
   
 
-  const rows = data.map((row) => ({
-    id: row.id,
-    address: row.address,
-    birth_date: row.birth_date,
-    sex: row.sex,
-  }));
+  // const rows = data.map((row) => ({
+  //   id: row.id,
+  //   address: row.address,
+  //   birth_date: row.birth_date,
+  //   sex: row.sex,
+  // }));
 
   return (
-    <div style={{ height: "100%", width: "100%",display:'flex'}} className='flex-column'>
-      <div className="d-flex">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-      </div>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-      />
-    </div>
+    // <div style={{ height: "100%", width: "100%",display:'flex'}} className='flex-column'>
+    //   <div className="d-flex">
+    //     <div>1</div>
+    //     <div>2</div>
+    //     <div>3</div>
+    //   </div>
+    //   <DataGrid
+    //     rows={rows}
+    //     columns={columns}
+    //     pageSize={10}
+    //     rowsPerPageOptions={[10]}
+    //   />
+    // </div>
+    <div></div>
   );
 }
 
