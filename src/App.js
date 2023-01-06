f29f146f0dfccd2a44fcc74143f7765dd55a194c
Edwin574 <owinoedwin574@gmail.com>
2023-01-06 22:58:36 +0300

    using dummy data

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="AppGlass">
         <Sidebar />
         <Customers></Customers>
-        <MainDash/>
+        {/* <MainDash/> */}
         <RightSide />
       </div>
     </div>
--- src/components/Customers/CustomerView.jsx
+++ src/components/Customers/CustomerView.jsx
@@ -11,11 +11,11 @@ function Customers() {
   useEffect(() => {
     async function getData() {
       await axios
-        .get("http://127.0.0.1:8000/auth/users")
+        .get("https://www.balldontlie.io/api/v1/players")
         .then((response) => {
           // check if the data is populated
-          console.log(response.data);
-          setData(response.data.results);
+          console.log(response.data.data);
+          setData(response.data.data);
           // you tell it that you had the result
           setLoadingData(false);
         });
@@ -26,51 +26,77 @@ function Customers() {
     }
   }, []);
 
-  // const columns = [
-  //   {
-  //     field: "id",
-  //     headerName: "ID",
-  //     width: 50,
-  //   },
-  //   {
-  //     field: "address",
-  //     headerName: "Address",
-  //     width: 150,
-  //   },
-  //   {
-  //     field: "birth_date",
-  //     headerName: "DOB",
-  //     width: 150,
-  //   },
-  //   {
-  //     field: "sex",
-  //     headerName: "Gender",
-  //     width: 100,
-  //   }]
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 50,
+    },
+    {
+      field: "first_name",
+      headerName: "First Name",
+      width: 100,
+    },
+    {
+      field: "last_name",
+      headerName: "Last Name",
+      width: 150,
+    },
+    {
+      field: "position",
+      headerName: "Position",
+      width: 80,
+    },
+    {
+      field: "team_name",
+      headerName: "Team Name",
+      width: 180,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      width: 150,
+    },
+    {
+      field: "conference",
+      headerName: "Conference",
+      width: 100,
+    },
+    {
+      field: "division",
+      headerName: "Division",
+      width: 100,
+    }
+  ]
   
 
-  // const rows = data.map((row) => ({
-  //   id: row.id,
-  //   address: row.address,
-  //   birth_date: row.birth_date,
-  //   sex: row.sex,
-  // }));
+  const rows = data.map((row) => ({
+    id: row.id,
+    first_name: row.first_name,
+    last_name: row.last_name,
+    position: row.position,
+    team_name: row.team.full_name,
+    city: row.team.city,
+    conference: row.team.conference,
+    division: row.team.division,
+  }));
 
   return (
-    // <div style={{ height: "100%", width: "100%",display:'flex'}} className='flex-column'>
-    //   <div className="d-flex">
-    //     <div>1</div>
-    //     <div>2</div>
-    //     <div>3</div>
-    //   </div>
-    //   <DataGrid
-    //     rows={rows}
-    //     columns={columns}
-    //     pageSize={10}
-    //     rowsPerPageOptions={[10]}
-    //   />
-    // </div>
-    <div></div>
+    <div style={{ height: "100%", width: "100%",display:'flex'}} className='flex-column'>
+      <div className="d-flex">
+        <div><h1>CUSTOMER DATA</h1></div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <DataGrid
+        rows={rows}
+        rowHeight={25}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+      />
+    </div>
+   
   );
 }
 
