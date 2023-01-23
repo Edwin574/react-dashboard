5073210c3f17310abbe0c0053801099e0eb5f8ed
Edwin574 <owinoedwin574@gmail.com>
2023-01-23 22:02:47 +0300

    style driver page

--- src/components/Drivers/Drivers.js
+++ src/components/Drivers/Drivers.js
@@ -1,9 +1,10 @@
 import React from "react"
+import "./Driver.css"
 
 function Driver() {
     
     return (
-        <div><h1> Hello Driver Page</h1></div>
+        <div className="driver-main"><h1> Hello Driver Page</h1></div>
     )
 }
 
